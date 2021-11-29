d629ebcc1ec7df2025c4ba2f606eb57b2e9ec7ec
Nuno Cunha <nunorafael_6@hotmail.com>
2021-11-29 01:42:18 +0000

    Add validations

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -25,7 +25,13 @@ export class TasksService {
   }
 
   public getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task with id: ' + id);
+    }
+
+    return task;
   }
 
   public createTask(createTaskDto: CreateTaskDto): Task {
@@ -43,6 +49,7 @@ export class TasksService {
   }
 
   public deleteTaskById(id: string): void {
+    this.getTaskById(id);
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
